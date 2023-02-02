ba11237f167f392cddd4c2078f41557038c4191d
Boschio <boschio91@gmail.com>
2023-02-02 14:28:54 -0500

    Added image of me and some more details. Created new component sand pages as a start.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'James A. Bosch';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Hire me.';
+export const RESUME: string = "/resume.pdf";
+export const SELFIE: string = "/me.jpg";
